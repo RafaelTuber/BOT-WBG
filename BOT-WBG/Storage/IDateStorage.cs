24e3af85efa0aa63cbbb83caed038baba32d6a88
RafaelTuber <35770666+RafaelTuber@users.noreply.github.com>
2018-11-21 21:57:34 -0200

    Add IDataSorage interface and InMemoryImplementation
    IDataStorage.cs and InMemoryStorage.cs

---
+++ BOT-WBG/Storage/IDateStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOT_WBG.Storage
+{
+    public interface IDateStorage
+    {
+        void StoreObject(object obj, string key);
+
+        T RestoreObject<T>(string key);
+    }
+}
---
+++ BOT-WBG/Storage/Implementation/InMemoryStorage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOT_WBG.Storage.Implementation
+{
+    public class InMemoryStorage : IDateStorage
+    {
+        private readonly Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+
+        public void StoreObject(object obj, string key)
+        {
+            if (_dictionary.ContainsKey(key)) return;
+            _dictionary.Add(key, obj);
+        }
+
+        public T RestoreObject<T>(string key)
+        {
+            if (!_dictionary.ContainsKey(key)) throw new ArgumentException($"A chave fornecida '{key}' não foi encontrada.");
+
+            return (T)_dictionary[key];
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("Olá!");
+        }
+    }
+}
+
