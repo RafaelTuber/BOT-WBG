5f8cdf1b15e68dab2ecf68c2f8abb2e4175d77ac
RafaelTuber <35770666+RafaelTuber@users.noreply.github.com>
2018-11-21 19:12:58 -0200

    first commit

---
+++ BOT-WBG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BOT_WBG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
